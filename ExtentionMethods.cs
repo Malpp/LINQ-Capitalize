11f9d4ce64369db65804207e48812d87a1939262
Malpp <cpt.ortan@gmail.com>
2016-11-01 13:16:43 -0400

    Init

---
+++ ExtentionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods
+{
+    public static class MyExtensions
+    {
+        public static string Capitalize(this String str)
+        {
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
